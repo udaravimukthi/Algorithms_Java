eed42dbc924a5548159078fa9ce3be83c28e3f28
Udara Vimukthi <uvlakshan@gmail.com>
2021-10-12 23:58:51 +0530

    Added the code part

---
+++ Part1/ReverseString/ReverseString.java
@@ -0,0 +1,16 @@
+//reverse
+
+//package Part1.ReverseString;
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input the word \n");
+        String s = sc.nextLine();
+        System.out.print("The original word:" + s);
+
+        String reverse = "";
+
+    }
+}
